8d0cb09e970f4a96b171feafb33dea6b33c092f8
raselmondol2344 <raselmondream245@gmail.com>
2026-04-30 01:01:10 +0600

    first deploy for json data link

--- src/app/all-tiles/page.jsx
+++ src/app/all-tiles/page.jsx
@@ -5,6 +5,7 @@ const allTilespage = async() => {
     const res = await fetch("http://localhost:3000/data.json")
     const tiles = await res.json();
     console.log(tiles);
+    
     return (
         <div className="container max-w-7xl mx-auto">
             <h1 className="text-2xl font-bold m-10">All Tiles</h1>
