3ae847ce7850e870d3c8afd408d54ce894716240
raselmondol2344 <raselmondream245@gmail.com>
2026-04-30 20:28:58 +0600

    github issu

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,15 +3,18 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
+await client.connect();
 const db = client.db("tiles_gallery");
 
+
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
 
-  baseURL: process.env.BETTER_AUTH_URL,
+ 
    emailAndPassword: { 
     enabled: true, 
 
