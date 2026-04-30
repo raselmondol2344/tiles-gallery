767cf41eeb6965a18709913356f0294f908810d6
raselmondol2344 <raselmondream245@gmail.com>
2026-04-30 15:29:25 +0600

    register page added

--- src/app/logIn/page.jsx
+++ src/app/logIn/page.jsx
@@ -1,12 +1,54 @@
+"use client"
+import { useForm } from "react-hook-form"
+import Link from "next/link";
+import { authClient } from "@/lib/auth-client";
 
 
-const logInpage = () => {
+
+const loginpage = () => {
+    const { register, handleSubmit, formState: { errors }, } = useForm()
+
+    const handleLogindatafun = async (data) => {
+        //console.log(data);
+
+        const { data:res, error } = await authClient.signIn.email({
+            email: data.email, 
+            password: data.password,
+            rememberMe: true,
+            callbackURL: "/",
+        });
+        console.log(res,error);
+    }
+
     return (
-        <div>
-           <h1>log in page</h1>
-            
+        <div className="container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100">
+            <div className=" rounded-xl bg-white p-20">
+                <h2 className="font-semibold text-lg mb-5">Login your account </h2>
+                <form className="space-y-5" onSubmit={handleSubmit(handleLogindatafun)}>
+
+                    <fieldset className="fieldset ">
+                        <legend className="fieldset-legend">Email</legend>
+                        <input type="email" className="input" placeholder="Enter your email" {...register("email", { required: "provite a valid email" })} />
+                        {
+                            errors.email && <p className="text-red-500">{errors.email.message}</p>
+                        }
+
+
+
+                        <legend className="fieldset-legend">Password</legend>
+                        <input type="password" className="input" placeholder="Enter your password" {...register("password", { required: "provite a valid password" })} />
+                        {
+                            errors.password && <p className="text-red-500">{errors.password.message}</p>
+                        }
+
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+
+                </form>
+                <p>have not any account ! <Link href={'/register'} className="text-blue-500">register</Link></p>
+            </div>
         </div>
     );
 };
 
-export default logInpage;
+export default loginpage;
---
+++ src/app/register/page.jsx
@@ -0,0 +1,93 @@
+"use client"
+import { useForm } from "react-hook-form"
+import Link from "next/link";
+import { url } from "better-auth";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+
+
+const registerpage = () => {
+
+    const router = useRouter();
+    const { register, handleSubmit, formState: { errors }, } = useForm()
+
+    const handleRegisterdatafun = async (data) => {
+        // console.log(data);
+
+        const { email, name, photo_url, password } = data;
+        //console.log(name, email, password, photo_url);
+
+
+
+        const { data: res, error } = await authClient.signUp.email({
+            name: name,
+            email: email,
+            password: password,
+            image: photo_url,
+            callbackURL: '/logIn',
+        });
+
+         console.log(res,error);
+
+         if(error){
+            alert(error.message)
+         }
+         if(res){
+            alert("signUp sucessfull")
+            router.push("/logIn");
+         }
+    }
+
+
+
+    return (
+        <div className="container mx-auto  flex justify-center items-center">
+            <div className=" rounded-xl bg-slate-200 py-10 px-20 mt-10 ">
+                <h2 className="font-semibold text-lg mb-5">Create a account </h2>
+                <form className="space-y-5" onSubmit={handleSubmit(handleRegisterdatafun)}>
+
+                    <fieldset className="fieldset ">
+
+                        <legend className="fieldset-legend">Name</legend>
+                        <input type="text" className="input" placeholder="Enter your name" {...register("name", { required: "provite a valid name" })} />
+                        {
+                            errors.name && <p className="text-red-500">{errors.name.message}</p>
+                        }
+
+
+                        <legend className="fieldset-legend">photo URL</legend>
+                        <input type="text" className="input" placeholder="Enter your photo url" {...register("photo_url", { required: "provite a valid photo url" })} />
+                        {
+                            errors.photo_url && <p className="text-red-500">{errors.photo_url.message}</p>
+                        }
+
+
+
+
+
+                        <legend className="fieldset-legend">Email</legend>
+                        <input type="email" className="input" placeholder="Enter your email" {...register("email", { required: "provite a valid email" })} />
+                        {
+                            errors.email && <p className="text-red-500">{errors.email.message}</p>
+                        }
+
+
+
+                        <legend className="fieldset-legend">Password</legend>
+                        <input type="password" className="input" placeholder="Enter your password" {...register("password", { required: "provite a valid password" })} />
+                        {
+                            errors.password && <p className="text-red-500">{errors.password.message}</p>
+                        }
+
+                        <button className="btn btn-neutral mt-4">create your account</button>
+                    </fieldset>
+
+                </form>
+                <p>alrady have a account ! <Link href={'/logIn'} className="text-blue-500">Login</Link></p>
+            </div>
+        </div>
+    );
+};
+
+export default registerpage;
