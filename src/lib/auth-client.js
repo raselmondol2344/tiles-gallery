5b772aac22b5d9725384bb1730e764dc905dfdf8
raselmondol2344 <raselmondream245@gmail.com>
2026-05-02 13:13:34 +0600

    url chnage

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,9 +1,10 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://tiles-gallery-akho.vercel.app",
+    baseURL: "https://tiles-gallery-lry3.vercel.app",
     
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
+
 
