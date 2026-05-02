e4538bff3c40d65e906523aaa2f0d1df59043b0e
raselmondol2344 <raselmondream245@gmail.com>
2026-05-02 12:09:41 +0600

    some issu soleve

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,13 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
+
+await client.connect();
 
 const db = client.db("tiles_gallery");
 
